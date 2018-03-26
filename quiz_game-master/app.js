04a9ef44e5a30bae06dcbcca19c9fd7857f1dacc
Jonathan Saona <31675526+jsaona@users.noreply.github.com>
2018-03-26 17:33:34 -0400

    fix

---
+++ quiz_game-master/app.js
@@ -0,0 +1,44 @@
+//Global variables
+
+//buttons
+//function myFunction() {
+//    var x = document.getElementById("myRadio");
+//    x.checked = true;
+//   
+//}
+
+
+function check() {
+    document.getElementById("lb").checked = true;
+}
+function uncheck() {
+    document.getElementById("lb").checked = false;
+}
+
+
+function check() {
+    document.getElementById("rb").checked = true;
+}
+function uncheck() {
+    document.getElementById("rb").checked = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//result for differnt alignment status
