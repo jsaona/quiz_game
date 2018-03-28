1a531a2e2a0f83dfb5d78b47e02f71a7fd1ac9f0
Jonathan Saona <31675526+jsaona@users.noreply.github.com>
2018-03-28 10:35:46 -0400

    updated

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-//array for questions
-
-var qArray = [
-  ["Question1:Do you get very angry when someone kills you in a game.?", "It's part of the game no biggie.", "REVENGE!"], 
-  ["Question2:Are you a Casual or Hardcore Gamer?", "Casual!", "Hardcore!"], 
-  ["Question3:Do you follow the rules or use exploits?", "I follow the rules for fair play.", "Exploits are part of the game."]
-  ["Question4:Do you use mods?", "No, I want the vanilla experience.", "Yeah, mods make the game better."]
-  ["Question5:Do you like to taunt your opponents", "I want to relax and enjoy the game.", "Psychological warfare is a legit tactic."]
-];
 
 
-//write to the index
-//used a for loop to repeat process till all questions have been displayed.
-for (var i = 0; i < qArray.length; i++){
-  document.body.appendChild("<form><span class='question'>" + qArray[i][0] + "</span><br>");
-  for (var x = 1; x < 4; x++){
-    document.body.appendChild("<input type='radio' class='answer' name='answer' value='" + qArray[i][x] + "'>" + questionArray[i][x] + "");
-  }
-  document.body.appendChild("</form><br>");
+
+//FUNCTION
+function check() {
+    document.getElementsByTagName("input").checked = true;
+}
+function uncheck() {
+    document.getElementsByTagName("input").checked = false;
 }
 
-// all possible combination for answers
-// total combination for questions are 10, there are 5 questions and 2 answers each. 
-//result will be output an alert window with the alignment picture.
-var alignment = [
-    
-]
+document.getElementById("button").onclick = function () {
+    gamerScore.lawfulGood++;
+    console.log(gamerScore);
+};
+
+//SCORE
+var gamerScore = {
+    lawfulGood: 0,
+    lawfulEvil: 0,
+    neutral: 0,
+    chaoticGood: 0,
+    chaoticEvil: 0
+};
 
 
 
@@ -40,14 +37,10 @@ var alignment = [
 
 
 
-//
-//function check() {
-//    document.getElementById("lb").checked = true;
-//}
-//function uncheck() {
-//    document.getElementById("lb").checked = false;
-//}
-//
-//function myFunction() {
-//    document.getElementById("button").innerHTML = "this is a test";
-//}
+
+
+
+
+
+
+//result for differnt alignment status
